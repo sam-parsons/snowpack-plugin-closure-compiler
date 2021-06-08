03ea8ff4fb72d48b3e60c1655df69af5c2873aa6
Sam Parsons <samparsons269@gmail.com>
2021-06-08 15:08:57 -0400

    basic plugin testing

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -3,6 +3,6 @@ const plugin = require('../plugin');
 describe('snowpack template plugin', () => {
   test('invokes plugin and returns correctly shaped object without error', () => {
     const obj = plugin();
-    expect(obj.name).toEqual('snowpack-plugin-template');
+    expect(obj.name).toEqual('snowpack-plugin-closure-compiler');
   });
 });
