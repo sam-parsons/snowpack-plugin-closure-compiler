775f1e529217f4d69db025f57d153d7aeb6c8acc
Sam Parsons <samparsons269@gmail.com>
2021-06-10 16:43:22 -0400

    modularized separate file generation to util directory

--- plugin.js
+++ plugin.js
@@ -1,7 +1,8 @@
 const { compiler } = require('google-closure-compiler');
 const fs = require('fs');
 const glob = require('glob');
-const bundle = require('./util/bundle.js');
+const generateBundle = require('./util/generateBundle.js');
+const generateSeparateFiles = require('./util/generateSeparateFiles.js');
 
 module.exports = function plugin(config, snowpackOptions) {
   return {
@@ -11,48 +12,16 @@ module.exports = function plugin(config, snowpackOptions) {
       const files = glob.sync(config.buildOptions.out + '/**/*.js');
 
       if (!snowpackOptions.bundle) {
-        const compilerInstances = files.map((file, index) => {
-          return new compiler({
-            js: file,
-            compilation_level: snowpackOptions.compilationLevel || 'SIMPLE',
-            language_in: snowpackOptions.languageIn || 'ECMASCRIPT_NEXT',
-            language_out: snowpackOptions.languageOut || 'ECMASCRIPT5',
-          });
-        });
-
-        const processPromises = compilerInstances.map((instance, index) => {
-          // save instance of child process to hang node process
-          const compilerProcess = instance.run((exitCode, stdOut, stdErr) => {
-            // handle user specified output file
-            // grab from compiler instance
-            const inputFile = instance.commandArguments
-              .filter((arg) => {
-                return arg.startsWith('--js');
-              })[0]
-              .slice(5);
-
-            fs.writeFile(inputFile, stdOut, (err) => {
-              if (err) log(err);
-            });
-          });
-
-          // return promise to wait for compiler completion
-          return new Promise((res, rej) => {
-            compilerProcess.on('exit', (arg) => {
-              log('closure compiler complete');
-              res();
-            });
-            compilerProcess.on('error', (err) => {
-              log('closure compiler err', err);
-              rej();
-            });
-          });
-        });
-
-        return Promise.all(processPromises);
+        return generateSeparateFiles(
+          files,
+          config,
+          snowpackOptions,
+          compiler,
+          log
+        );
       }
 
-      return bundle(files, config, snowpackOptions, compiler, log);
+      return generateBundle(files, config, snowpackOptions, compiler, log);
     },
   };
 };
