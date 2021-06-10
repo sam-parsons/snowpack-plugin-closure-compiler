fab0dae697f5223521e9d9c1dfcafbc33c15a392
Sam Parsons <samparsons269@gmail.com>
2021-06-10 17:09:47 -0400

    modularized generator options and added ternary return statement

--- plugin.js
+++ plugin.js
@@ -1,5 +1,3 @@
-const { compiler } = require('google-closure-compiler');
-const fs = require('fs');
 const glob = require('glob');
 const generateBundle = require('./util/generateBundle.js');
 const generateSeparateFiles = require('./util/generateSeparateFiles.js');
@@ -11,17 +9,11 @@ module.exports = function plugin(config, snowpackOptions) {
       // retreive all javascript files in build directory
       const files = glob.sync(config.buildOptions.out + '/**/*.js');
 
-      if (!snowpackOptions.bundle) {
-        return generateSeparateFiles(
-          files,
-          config,
-          snowpackOptions,
-          compiler,
-          log
-        );
-      }
+      const generatorOptions = [files, config, snowpackOptions, log];
 
-      return generateBundle(files, config, snowpackOptions, compiler, log);
+      return snowpackOptions.bundle
+        ? generateBundle(...generatorOptions)
+        : generateSeparateFiles(...generatorOptions);
     },
   };
 };
