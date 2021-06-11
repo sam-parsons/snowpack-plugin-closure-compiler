e5e18fca2c789d11391ecf692e179eeb293cf09b
Sam Parsons <samparsons269@gmail.com>
2021-06-10 23:07:39 -0400

    refactored generator imports on plugin

--- plugin.js
+++ plugin.js
@@ -1,19 +1,18 @@
 const glob = require('glob');
-const generateBundle = require('./util/generateBundle.js');
-const generateSeparateFiles = require('./util/generateSeparateFiles.js');
+const { generateBundle, generateSeparate } = require('./util/generators.js');
 
 module.exports = function plugin(config, snowpackOptions) {
   return {
     name: 'snowpack-plugin-closure-compiler',
     async optimize({ log }) {
-      // retreive all javascript files in build directory
+      // retrieve all javascript files in build directory
       const files = glob.sync(config.buildOptions.out + '/**/*.js');
 
       const generatorOptions = [files, config, snowpackOptions, log];
 
       return snowpackOptions.bundle
         ? generateBundle(...generatorOptions)
-        : generateSeparateFiles(...generatorOptions);
+        : generateSeparate(...generatorOptions);
     },
   };
 };
