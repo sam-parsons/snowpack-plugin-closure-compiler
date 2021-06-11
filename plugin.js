172a76e851ac3cd0e31f0a6f9a12197ecbd04372
Sam Parsons <samparsons269@gmail.com>
2021-06-10 23:37:07 -0400

    refactored glob call to fs.readdirSync

--- plugin.js
+++ plugin.js
@@ -1,4 +1,4 @@
-const glob = require('glob');
+const fs = require('fs');
 const { generateBundle, generateSeparate } = require('./util/generators.js');
 
 module.exports = function plugin(config, snowpackOptions) {
@@ -6,7 +6,14 @@ module.exports = function plugin(config, snowpackOptions) {
     name: 'snowpack-plugin-closure-compiler',
     async optimize({ log }) {
       // retrieve all javascript files in build directory
-      const files = glob.sync(config.buildOptions.out + '/**/*.js');
+      // form build directory absolute path
+      const bundleDir = config.mount[config.root + '/src'].url;
+      const jsBuildDir = config.root + '/build' + bundleDir;
+      // create array of absolute paths to js files
+      const files = fs
+        .readdirSync(jsBuildDir)
+        .filter((file) => file.endsWith('.js'))
+        .map((file) => jsBuildDir + '/' + file);
 
       const generatorOptions = [files, config, snowpackOptions, log];
 
