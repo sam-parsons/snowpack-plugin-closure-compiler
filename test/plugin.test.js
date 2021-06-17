355acbfb0ae953551d51485a5c3cc3719f90baec
Sam Parsons <samparsons269@gmail.com>
2021-06-17 11:51:49 -0400

    refactored existing tests with mockConfig and log arguments

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,14 +1,16 @@
 const plugin = require('../plugin');
+const mockConfig = require('../mocks/snowpack.config.mock');
 
 describe('snowpack template plugin', () => {
   test('invokes plugin and returns correctly shaped object without error', () => {
-    const obj = plugin();
+    const obj = plugin(mockConfig, {});
     expect(obj.name).toEqual('snowpack-plugin-closure-compiler');
   });
 
   test('optimize method returns a new Promise', () => {
-    const obj = plugin();
-    expect(obj.optimize()).toBeInstanceOf(Promise);
-    expect(obj.optimize().constructor.name).toBe('Promise');
+    const obj = plugin(mockConfig, {});
+    const result = obj.optimize({ log: console.log });
+    expect(result).toBeInstanceOf(Promise);
+    expect(result.constructor.name).toBe('Promise');
   });
 });
